7a44c67449426864c4916f07ee859102acbb9bb9
courtneyschiffman <courtneys@berkeley.edu>
2015-12-08 15:53:14 -0800

    CS Add Documentation

--- R/zFun.R
+++ R/zFun.R
@@ -1,2 +1,10 @@
-zFun <-
-function(x,g) { }
+#' Helper function to get the intersection of tangents at $x_{j}$ and $x_{j+1}$
+#' 
+#' @param x A vector of x values of your points. The first element in the vector
+#' has smaller x-coordinate. 
+#' @param g A fucntion user wants to generate samples from. This function is 
+#' used to calculate h(x)=ln(g(x))
+#' @return intersection of tangents of $x_{j}$ and $x_{j+1}$ named $z_{j}$
+#' @export
+
+zFun <-function(x,g) { }
