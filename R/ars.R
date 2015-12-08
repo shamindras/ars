c6a83ee63f969288d6c862fc5e7a09524fd469cd
Shamindra Shrotriya <shamindra.shrotriya@gmail.com>
2015-12-08 15:48:58 -0800

    SS: Added Roxygen2 tags for ars, checkAccept, checkLC, choose2 functions

--- R/ars.R
+++ R/ars.R
@@ -1,2 +1,9 @@
-ars <-
-function(n,g,D) { }
+#' Main function to carry out simulation 
+#' 
+#' @param n A number indicates how many accepted points user wants to generate. 
+#' @param g A function user wants to generate samples from. 
+#' @param D A vector with two elements indicates the domain of the sample 
+#' generation.
+#' @return n accepted sampling points. 
+#' @export
+ars <- function(n,g,D) { }
--- R/checkLC.R
+++ R/checkLC.R
@@ -1,2 +1,6 @@
-checkLC <-
-function(f) { }
+#' Helper function to check the log concavity
+#' 
+#' @param f A function that requires the log concavity check 
+#' @return boolean value TRUE or FALSE to indicate the log concavity 
+#' @export
+checkLC <- function(f) { }
