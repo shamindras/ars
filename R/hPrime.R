b9cdb84023db813e4d2e948ea7c362cebc4ad3d7
courtneyschiffman <courtneys@berkeley.edu>
2015-12-09 13:15:06 -0800

    CS Fix some documentation

--- R/hPrime.R
+++ R/hPrime.R
@@ -1,9 +1,14 @@
 #' Helper function to get first derivative of h(x)
 #' 
-#' @param g A function user wants to generate samples from. This function is 
+#' @param inp_g A function of x which the user wants to generate samples from. This function is 
 #' used to calculate h(x)=ln(g(x)).
 #' @param x A number indicates the x-axis of the points 
 #' @return the first derivative of h(x) at the point x 
 #' @export
 
-hPrime <-function(g,x) { }
+g<-function(x) x^2
+hPrime <-function(inp_g,x) {
+  inp_g(x)+2
+}
+
+hPrime(g,x)
--- R/sFun.R
+++ R/sFun.R
@@ -1,11 +1,16 @@
-#' Helper function to get function $s_{k}(x)$
+#' Helper function to get function \eqn{s_k(x)}
 #' 
-#' @param x A vector of x values of all points 
-#' @param z A vector of z values of all points and we should be able to get 
-#' the index of z
+#' @param inp_x A vector of inp_x values of all points
+#' @param z A vector of z values of all points and we should be able to get the 
+#'   index of z
 #' @param g A function user wants to generate samples from. This function is 
-#' used to calculate h(x)=ln(g(x))
-#' @return the function $s_{k}(x)$ for x in the interval [$z_{j-1}$,$z_{j}$]
+#'   used to calculate h(x)=ln(g(x))
+#' @param D a numeric vector indicating the domain of the g function. It is
+#' possible to specify D=c(-Inf,Inf).
+#' @return the function $s_{k}(x)$ for all x in the domain of g.
 #' @export
 
-sFun <- function(x,z,g) { }
+sFun <- function(x,inp_x,z,g,D) { 
+  f <- function(y) exp(uFun(x=y,inp_x=inp_x,z=z,g=g,D=D))
+  f(y=x)/(integrate(f,lower=D[1],upper=D[2])[[1]])
+}
