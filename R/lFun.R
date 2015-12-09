1248d12133545921f66a7eb3db30d67e1ec787dc
courtneyschiffman <courtneys@berkeley.edu>
2015-12-08 16:05:33 -0800

    CS-lfun-sfun

--- R/lFun.R
+++ R/lFun.R
@@ -1,2 +1,11 @@
-lFun <-
-function(x,g) { }
+#' Helper function to get the lower bound linear function $l_{k}(x)$
+#' 
+#' @param x A vector of x values of all points and we should be able to get the
+#' index of x 
+#' @param g A function user wants to generate samples from. This function is 
+#' use to calculate h(x)=ln(g(x))
+#' @return the lower bound linear function $l_{k}(x)$ for x in the interval 
+#' [$x_{j}$,$x_{j+1}$]
+#' @export
+
+lFun <- function(x,g) { }
