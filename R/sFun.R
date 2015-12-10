2aef90e8e64dcb6a2b9082e2f2d5fb0825f91a62
courtneyschiffman <courtneys@berkeley.edu>
2015-12-09 22:11:34 -0800

    CS sFun changes

--- R/sFun.R
+++ R/sFun.R
@@ -1,11 +1,14 @@
 #' Helper function to get function $s_{k}(x)$
 #' 
-#' @param x A vector of x values of all points 
-#' @param z A vector of z values of all points and we should be able to get 
+#' @param z_int A vector of z values of all points and we should be able to get 
 #' the index of z
-#' @param g A function user wants to generate samples from. This function is 
-#' used to calculate h(x)=ln(g(x))
+#' @param uFun_out A list of functions, the output from uFun.
 #' @return the function $s_{k}(x)$ for x in the interval [$z_{j-1}$,$z_{j}$]
 #' @export
 
-sFun <- function(x,z,g) { }
+sFun <- function(z_int,uFun_out) {
+  c <- function(i) integrate(uFun_out[[i]],z_int[[i]][1],z_int[[i]][2])[[1]]
+  constant <- sum(sapply(seq(1:length(z_int)),c))
+  
+
+}
