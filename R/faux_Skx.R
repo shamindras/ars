604dfc713a403d25758203c796b3c71a166eeb1a
courtneyschiffman <courtneys@berkeley.edu>
2015-12-11 12:40:36 -0800

    CS SampleSkx

--- R/faux_Skx.R
+++ R/faux_Skx.R
@@ -1,9 +1,10 @@
-#' Helper function to create piecewise function {s_k(x)}
-#' @param inp_uintervallist A list of intervals between z values, as in the output
-#' from uInterval.
+#' Helper function to create piecewise function \eqn{s_{k}(x)}
+#' @param inp_uintervallist A list of intervals between z values, as in the 
+#' output from uInterval.
 #' @param inp_ufunlist A list of functions, the output from uFun.
-#' @return the function $s_{k}(x)$ for all x in the domain of g, in list format,
-#' where each element of the list is one element of the piecewise function.
+#' @return the function \eqn{s_{k}(x)} for all x in the domain of g, in list
+#' format, where each element of the list is one element of the piecewise 
+#' function.
 #' @export
 faux_Skx <- function(inp_uintervallist, inp_ufunlist) {
   
@@ -23,8 +24,9 @@ faux_Skx <- function(inp_uintervallist, inp_ufunlist) {
     stop("Intervals in inp_uintervallist must be two dimensional vectors")
   }
   
-  # check that the first element of inp_ufunlist is a function
-  if(!(class(inp_ufunlist[[1]])=="function")){
+  # check that every element of inp_ufunlist is a function
+  if(!all.equal(lapply(inp_ufunlist,class),
+                as.list(rep("function",length(inp_ufunlist))))){
     stop("inp_ufunlist must be a list of functions")
   }
   
