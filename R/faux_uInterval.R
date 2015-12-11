262cae95fbd542faea7a39e0906a6c19f49417d1
courtneyschiffman <courtneys@berkeley.edu>
2015-12-10 20:16:20 -0800

    CS uInterval finished

--- R/faux_uInterval.R
+++ R/faux_uInterval.R
@@ -1,8 +1,10 @@
 #' Helper function to create the z intervals.
 #' @param inp_z A vector of z values, such as the output from faux_Zj().
-#' @return  A list, with each element of the list an interval 2 z points
+#' @return  A list, with each element of the list an interval between 2 z points
 #' @export
 
 faux_uInterval <- function(inp_z) {
-
+  z <- cbind(inp_z[1:(length(inp_z)-1)],inp_z[2:length(inp_z)])
+  faux_uInterval_out <- as.list(data.frame(t(z)))
+  return(faux_uInterval_out)
 }
