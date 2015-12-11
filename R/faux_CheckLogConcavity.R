b6607ef141a090a19ad521804057d01decf6acd3
Jonathan Larson <jonathan.larson@berkeley.edu>
2015-12-11 15:00:23 -0800

    JL Wrote function to check log concavity, and tests

--- R/faux_CheckLogConcavity.R
+++ R/faux_CheckLogConcavity.R
@@ -1,11 +1,42 @@
 #' Helper function to check the log concavity
 #' 
-#' @param inp_xvec A vector of 3 consecutive x values $(x_{1}, x_{2}, x_{3})$
-#'    in the domain of the user supplied function $g(x)$   
+#' @param inp_xvec A vector of 3 consecutive x values \eqn{(x_{1}, x_{2}, x_{3})}
+#'    in the domain of the user supplied function \eqn{g(x)}
 #' @param inp_gfun A function user wants to generate samples from. This function is 
 #'    used to calculate h(x)=ln(g(x)). We can then test the log-concavity
-#'    at the 3 x-coordinates $(x_{1}, x_{2}, x_{3})$
+#'    at the 3 x-coordinates \eqn{(x_{1}, x_{2}, x_{3})}
 #' @return boolean value TRUE or FALSE to indicate the log concavity 
 #' @export
 faux_CheckLogConcavity <- function(inp_xvec, inp_gfun){ 
+  
+  # inp_gfun must be a function
+  if(!is.function(inp_gfun)){
+    stop("inp_gfun must be a valid R function")
+  }
+  
+  # inp_xvec must be numeric and contain exactly 3 elements 
+  if(length(inp_xvec) != 3 | !is.numeric(inp_xvec)){
+    stop("inp_xvec must be exactly 3 numeric elements")
+  }
+  
+  # inp_xvec must be in ascending order
+  if(sum(!(sort(inp_xvec) == inp_xvec)) > 1){
+    stop("inp_xvec must be in ascending order")
+  }
+  
+  # Create h function from the g function that is input by the user
+  h_x <- faux_hx(inp_gfun)
+  
+  # Evaluate h at each value of x in the x vector parameter
+  y <- h_x(inp_xvec)
+  
+  # Calculate the gradients between the first two points and the last two points
+  g1 <- (y[2] - y[1]) / (inp_xvec[2] - inp_xvec[1])
+  g2 <- (y[3] - y[2]) / (inp_xvec[3] - inp_xvec[2])
+  
+  # Check if the first gradient is greater than or equal to the second,
+  # which will be the case if h is concave
+  CheckLogConcavity_out <- g1 >= g2
+  
+  return(CheckLogConcavity_out)
 }
